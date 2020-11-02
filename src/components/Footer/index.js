af7a945371046103c84197d5a708f18e23425e20
eduardobaia <eduardo_baia@msn.com>
2020-11-02 20:05:00 +0000

    add icons part container

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+// import {
+//     FooterContainer,
+//     FooterWrap,
+//     FooterLinksContainer,
+//     FooterLinksWrapper,
+//     FooterLinkItems,
+//     FooterLinkTitle,
+//     FooterLink
+// } from './FooterElements';
+
 import {
     FooterContainer,
     FooterWrap,
@@ -6,7 +17,13 @@ import {
     FooterLinksWrapper,
     FooterLinkItems,
     FooterLinkTitle,
-    FooterLink
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebSiteRights,
+    SocialIconLink,
+    SocialIcons
 } from './FooterElements';
 
 const Footer = () => {
@@ -33,8 +50,61 @@ const Footer = () => {
                             <FooterLink to="/signin"> Sign In</FooterLink>
 
                         </FooterLinkItems>
+
+
+                    </FooterLinksWrapper>
+
+
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle> Videos</FooterLinkTitle>
+                            <FooterLink to="/signin"> Home</FooterLink>
+                            <FooterLink to="/signin"> Services</FooterLink>
+                            <FooterLink to="/signin">Contact</FooterLink>
+                            <FooterLink to="/signin"> About us</FooterLink>
+                            <FooterLink to="/signin"> Sign In</FooterLink>
+
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle> Social Media</FooterLinkTitle>
+                            <FooterLink to="/signin"> Home</FooterLink>
+                            <FooterLink to="/signin"> Services</FooterLink>
+                            <FooterLink to="/signin">Contact</FooterLink>
+                            <FooterLink to="/signin"> About us</FooterLink>
+                            <FooterLink to="/signin"> Sign In</FooterLink>
+
+                        </FooterLinkItems>
+
+
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+
+
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'></SocialLogo>
+                        <WebSiteRights>IRON TEAM © {new Date().getFullYear()} All rights reserved.</WebSiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank"
+                                arial-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                                arial-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+
+                            <SocialIconLink href="/" target="_blank"
+                                arial-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconLink>
+
+
+
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+
             </FooterWrap>
         </FooterContainer>
     )
